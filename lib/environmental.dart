b8501dbcfb304e4ecb43a959f3a898ca0e7c9699
chikkub20@gmail.com <akhil7ancilmedia@gmail.com>
2026-08-14 14:37:41 +0530

    new commit

--- lib/environmental.dart
+++ lib/environmental.dart
@@ -3,8 +3,8 @@ String Dropbox_API_KEY = "7_cGSBjX6ZoAAAAAAAAAEt5EXDBMs7IXRv7hsM6S3mw";
 String Dropbox_APP_KEY = "dlrj13p1gea2j07";
 
 // Emailjs
-String Gmail_Service_ID = "service_66fig0h";
-String Gmail_Template_ID = "template_ymbj9px";
+String Gmail_Service_ID = "service_r119izw";
+String Gmail_Template_ID = "template_nuxn5pb";
 String Gmail_Public_KEY = "8sMAodMmeWJ1uzfXS";
 String Ngrok_KEY = "https://mitsuko-tigerish-evasively.ngrok-free.dev";
 String Vercel_url = "https://thevah.vercel.app/";
